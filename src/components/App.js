f5822a581ec4047da265da79b6cb820898a6beeb
Daniel Cheong <dcheo001@gmail.com>
2021-11-23 10:47:05 -0500

    commit tree c27fec012741246964890a8f5a5a73146fecbeba

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <p>'This is an about section'</p>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
